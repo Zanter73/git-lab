84bc927433ae70886968047deda3ef789cd843b0
Nathan <nb366421@ohio.edu>
2022-09-06 17:30:57 -0400

    git lab program

---
+++ 2400/git-lab-program.cc
@@ -0,0 +1,16 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Nathan Brooks
+ *        Date: 09/06/2022
+ */
+ 
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
